fe64f01686b08b6f3aa2aef90a0c5307dd37a372
Bruno Correia <bruno.h.c@hotmail.com>
2025-07-31 13:09:51 -0300

    projeto

---
+++ src/main/java/com/bruno/tarefa42/Tarefa42Application.java
@@ -0,0 +1,21 @@
+package com.bruno.tarefa42;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import io.github.cdimascio.dotenv.Dotenv;
+
+@SpringBootApplication
+public class Tarefa42Application {
+
+	public static void main(String[] args) {
+		
+		Dotenv dotenv = Dotenv.configure().load();
+        System.setProperty("DB_URL", dotenv.get("DB_URL"));
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+		
+		SpringApplication.run(Tarefa42Application.class, args);
+	}
+
+}
